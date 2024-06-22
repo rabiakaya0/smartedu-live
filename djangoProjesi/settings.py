e6eeefe665682f9f0bb99186a75b1bda335b50c1
Rabia Kaya <98751296+rabiakaya0@users.noreply.github.com>
2024-06-22 15:34:25 +0300

    settings.py değişiklikleri yapıldı

--- djangoProjesi/settings.py
+++ djangoProjesi/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -126,6 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
     os.path.join(BASE_DIR, '../node_modules/@fortawesome/fontawesome-free')
